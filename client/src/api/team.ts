6c02f1e8527db173eba3c2112b90e0d92fbea29c
Anarak404 <annaarak2@gmail.com>
2021-11-22 20:01:27 +0100

    feat(api): team api requests

---
+++ client/src/api/team.ts
@@ -0,0 +1,49 @@
+import { httpClient } from "./client";
+import {
+  IResultResponse,
+  ITeam,
+  ITeamDetails,
+  ITeamMembersRequest,
+  ITeamRequest
+} from "./types";
+
+export const getTeamsApi = async (): Promise<ITeam[]> => {
+  return httpClient.get("/app/teams/getTeams");
+};
+
+export const createTeamApi = async (data: ITeamRequest): Promise<ITeam> => {
+  return httpClient.post("/app/teams/create", data);
+};
+
+export const getTeamApi = async (id: number): Promise<ITeamDetails> => {
+  return httpClient.get(`/app/teams/${id}`);
+};
+
+export const deleteTeamApi = async (id: number): Promise<IResultResponse> => {
+  return httpClient.delete(`/app/teams/${id}`);
+};
+
+export const getMembersApi = async (id: number): Promise<ITeam[]> => {
+  return httpClient.get(`/app/teams/${id}/members`);
+};
+
+export const addMembersApi = async (
+  data: ITeamMembersRequest,
+  id: number
+): Promise<ITeamDetails> => {
+  return httpClient.post(`/app/teams/${id}/members`, data);
+};
+
+export const deleteMemberApi = async (
+  teamId: number,
+  userId: number
+): Promise<IResultResponse> => {
+  return httpClient.get(`/app/teams/${teamId}/members/${userId}`);
+};
+
+export const updateTeamApi = async (
+  data: ITeamRequest,
+  id: number
+): Promise<ITeam> => {
+  return httpClient.put(`/app/teams/${id}/update`, data);
+};
--- client/src/api/types.ts
+++ client/src/api/types.ts
@@ -7,7 +7,31 @@ export interface ISignUpRequest extends ISignInRequest {
   name: string;
 }
 
+export interface ITeamMembersRequest {
+  emails: string[];
+}
+
+export interface IResultResponse {
+  success: boolean;
+}
+
+export interface ITeamRequest {
+  name: string;
+  photo: string;
+}
+
 export interface IUser {
   name: string;
   email: string;
+  photo: string;
+}
+
+export interface ITeam {
+  name: string;
+  photo: string;
+  owner: IUser;
+}
+
+export interface ITeamDetails extends ITeam {
+  teamMembers: IUser[];
 }
